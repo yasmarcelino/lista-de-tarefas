0f3b52a59b477947b412473a47fe6a7717d0c26b
yasmarcelino <97450265+yasmarcelino@users.noreply.github.com>
2022-01-19 14:26:24 -0300

    Add files via upload

---
+++ JS/init.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC5-5NwdUMVT4MRHmJp6CLFDJwLGxfGVMk",
+    authDomain: "lista-de-tarefas-313ec.firebaseapp.com",
+    projectId: "lista-de-tarefas-313ec",
+    storageBucket: "lista-de-tarefas-313ec.appspot.com",
+    messagingSenderId: "262380680595",
+    appId: "1:262380680595:web:da778e5dfd545ce00fa466"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
